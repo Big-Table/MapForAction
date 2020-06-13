59187aace377265338acf011135912970806877f
arichards4814 <arichards4814@gmail.com>
2020-06-13 12:44:59 -0400

    Merged with incident branch

--- mfa-backend/db/migrate/20200613162642_drop_tweets.rb
+++
@@ -1,5 +0,0 @@
-class DropTweets < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :tweets 
-  end
-end
--- mfa-backend/db/migrate/20200613163205_drop_actions.rb
+++
@@ -1,5 +0,0 @@
-class DropActions < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :actions
-  end
-end
