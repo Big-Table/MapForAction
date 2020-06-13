9189ffc4eadb801eb4b9c3f877eebe55196dd8ed
arichards4814 <arichards4814@gmail.com>
2020-06-13 13:07:17 -0400

    added columns lat and lng for incidents

--- mfa-backend/db/schema.rb
+++ mfa-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_163331) do
+ActiveRecord::Schema.define(version: 2020_06_13_170529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_06_13_163331) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "lat"
+    t.string "lng"
   end
 
   create_table "tweets", force: :cascade do |t|
