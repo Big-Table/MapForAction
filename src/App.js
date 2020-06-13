3d3f58bf96670350f34290e1b9ceea450a1ae0d4
Steven Zhu <stevenzhu03@gmail.com>
2020-06-13 18:36:08 -0400

    [Feature]: added event listener for markers and map to set state in App

--- src/App.js
+++ src/App.js
@@ -5,22 +5,54 @@ import IncidentsContainer from "./containers/IncidentsContainer";
 import FlexColumn from "./Theme/FlexColumn";
 import FlexRow from "./Theme/FlexRow";
 import Map from "./Map";
-import IncidentForm from './components/IncidentForm'
+import IncidentForm from "./components/IncidentForm";
 
 class App extends React.Component {
+  state = {
+    incidents: [],
+    currentIncident: null,
+  };
+
+  deleteCurrentIncident = () => {
+    this.setState({
+      currentIncident: null,
+    });
+  };
+
+  setCurrentIncident = (incident) => {
+    this.setState({
+      currentIncident: incident,
+    });
+  };
+
+  setIncidents = (incidents) => {
+    this.setState({
+      ...this.state,
+      incidents,
+    });
+  };
+
   render() {
     return (
-      <Router >
-        <FlexColumn style={{width: "70vw", height: "100vh"}}>
-        {/* Map goes here */}
-            <Map />
+      <Router>
+        <FlexColumn style={{ width: "70vw", height: "100vh" }}>
+          {/* Map goes here */}
+          <Map
+            deleteCurrentIncident={this.deleteCurrentIncident}
+            setCurrentIncident={this.setCurrentIncident}
+            setIncidents={this.setIncidents}
+            incidents={this.state.incidents}
+          />
           {/* <IncidentForm></IncidentForm> */}
         </FlexColumn>
         <FlexColumn style={{ width: "30vw", height: "100vh" }}>
-        <Switch >
-          {/* Routes to different side pages go here */}
-            <Route path="/incidents" render={(routerProps) => < IncidentsContainer {...routerProps} />} />
-        </Switch>
+          <Switch>
+            {/* Routes to different side pages go here */}
+            <Route
+              path="/incidents"
+              render={(routerProps) => <IncidentsContainer {...routerProps} />}
+            />
+          </Switch>
         </FlexColumn>
       </Router>
     );
