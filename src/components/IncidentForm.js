d4f3f776edba57f25139b5f49a56b5e2c837effb
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2020-06-13 17:18:00 -0400

    Add incident form style, alert on form submission, index.html font-family

--- src/components/IncidentForm.js
+++ src/components/IncidentForm.js
@@ -1,22 +1,21 @@
 import React from "react";
 import "./IncidentForm.css";
-import { makeStyles } from '@material-ui/core'
-import { postIncidents} from '../requests/requests'
+import { makeStyles } from "@material-ui/core";
+import { postIncidents } from "../requests/requests";
 class IncidentForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       title: "",
       description: "",
-      lat: "", 
-      lng: "", 
+      lat: "",
+      lng: "",
       organization: "",
       petition: "",
       image_url: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    //bind addToDatabase function
   }
 
   handleChange(event) {
@@ -25,29 +24,25 @@ class IncidentForm extends React.Component {
     });
   }
 
-  //FUNCTION addToDatabase => to send created incident to the database
-
   handleSubmit(e) {
     e.preventDefault();
     console.log("Hello World");
-    postIncidents(this.state)
+    postIncidents(this.state);
     this.setState({
       title: "",
       description: "",
-      lat: "", 
-      lng: "", 
+      lat: "",
+      lng: "",
       organization: "",
       petition: "",
       image_url: "",
-    })
-    //invoke a function that will send created incident to the database
-    //clean state
-    //alert("This incidence has been reported, thank you for being proactive");
+    });
+    alert("This incidence has been reported, thank you for being proactive");
   }
 
   render() {
     return (
-      <>
+      <div id="incidentForm">
         <h2>Report an Incident</h2>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="title">
@@ -56,7 +51,8 @@ class IncidentForm extends React.Component {
           <input
             name="title"
             type="text"
-            title="Write the incident title"
+            maxlength="90"
+            placeholder="Write the incident title"
             aria-describedby="required-description"
             value={this.state.title}
             onChange={this.handleChange}
@@ -65,10 +61,12 @@ class IncidentForm extends React.Component {
           <label htmlFor="description">
             Explain what you saw<span className="required">*</span>
           </label>
-          <input
+          <textarea
+            id="descriptionInput"
             name="description"
             type="text"
-            title="The more details, the better!"
+            rows="5"
+            placeholder="The more details, the better!"
             aria-describedby="required-description"
             value={this.state.description}
             onChange={this.handleChange}
@@ -78,10 +76,10 @@ class IncidentForm extends React.Component {
             Latitude<span className="required">*</span>
           </label>
           <input
-            hidden 
+            hidden
             name="lat"
             type="text"
-            title="Enter street address or intersections"
+            placeholder="Enter street address or intersections"
             aria-describedby="required-description"
             value={this.state.lat}
             onChange={this.handleChange}
@@ -94,7 +92,7 @@ class IncidentForm extends React.Component {
             hidden
             name="lng"
             type="text"
-            title="Enter street address or intersections"
+            placeholder="Enter street address or intersections"
             aria-describedby="required-description"
             value={this.state.lng}
             onChange={this.handleChange}
@@ -104,7 +102,7 @@ class IncidentForm extends React.Component {
           <input
             name="organization"
             type="text"
-            title="Add a link to related organization"
+            placeholder="Add a link to related organization"
             value={this.state.organization}
             onChange={this.handleChange}
           />
@@ -113,7 +111,7 @@ class IncidentForm extends React.Component {
           <input
             name="petition"
             type="text"
-            title="Add a link to related petition"
+            placeholder="Add a link to related petition"
             value={this.state.petition}
             onChange={this.handleChange}
           />
@@ -128,12 +126,12 @@ class IncidentForm extends React.Component {
             accept=".png, .jpg, .jpeg"
           />
 
-          <input type="submit" value="Submit" />
-          <p aria-hidden="true" id="required-description">
+          <input type="submit" value="Submit" id="submitButton" />
+          {/* <p aria-hidden="true" id="required-description">
             <span className="required">*</span>Required field
-          </p>
+          </p> */}
         </form>
-      </>
+      </div>
     );
   }
 }
