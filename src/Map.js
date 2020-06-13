b78e1d56c4064ebd377d6d84e8365e2d98ed4673
Steven Zhu <stevenzhu03@gmail.com>
2020-06-13 13:09:49 -0400

    [Feature]: Created Test Marker and Styled Map

--- src/Map.js
+++ src/Map.js
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { Component, createRef } from "react";
+import mapStyles from "./mapStyles";
 
-import {
-  GoogleMap,
-  useLoadScript,
-  Marker,
-  InfoWindow,
-} from "@react-google-maps/api";
+class Map extends Component {
+  googleMapRef = createRef();
 
-const libraries = ["places"];
+  state = {
+    current: {
+      lat: 40.7278722,
+      lng: -73.9572483,
+    },
+  };
 
-const Map = (props) => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries,
-  });
+  componentDidMount() {
+    const googleScript = document.createElement("script");
+    googleScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCeB3fijFTmeINUL-CSVErtAIIfxv5LNxc&libraries=places`;
+    window.document.body.appendChild(googleScript);
 
-  if (loadError) return "Error";
-  if (!isLoaded) return "Loading...";
+    googleScript.addEventListener("load", () => {
+      this.googleMap = this.createGoogleMap();
+      this.marker = this.createMarker();
+    });
+  }
 
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
-};
+  createGoogleMap = () =>
+    new window.google.maps.Map(this.googleMapRef.current, {
+      zoom: 14,
+      center: this.state.current,
+      disableDefaultUI: true,
+      styles: mapStyles,
+    });
 
-export default GoogleMap;
+  createMarker = () =>
+    new window.google.maps.Marker({
+      position: { lat: 40.7278722, lng: -73.9572483 },
+      map: this.googleMap,
+      icon: {
+        path: window.google.maps.SymbolPath.CIRCLE,
+        fillColor: "#EBC700",
+        fillOpacity: 1,
+        strokeColor: "#EBC700",
+        strokeOpacity: 0.9,
+        strokeWeight: 1,
+        scale: 10,
+      },
+    });
+
+  render() {
+    return (
+      <div
+        id="google-map"
+        ref={this.googleMapRef}
+        style={{ width: "100vw", height: "100vh" }}
+      />
+    );
+  }
+}
+
+export default Map;
