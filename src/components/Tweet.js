0247087a64bbeef99d383e7e154c78331049a052
arichards4814 <arichards4814@gmail.com>
2020-06-13 17:07:06 -0400

    added tweet component

---
+++ src/components/Tweet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TweetEmbed from 'react-tweet-embed'
+
+
+const Tweet = props => {
+
+
+    return(
+        <TweetEmbed id={props.url.split("/")[5]} />
+    )
+}
+
+export default Tweet
