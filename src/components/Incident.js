18804cf0f3980d405929c30b80dc3b94f93a8a45
Steven Zhu <stevenzhu03@gmail.com>
2020-06-14 13:05:27 -0400

    styling change to incident card

--- src/components/Incident.js
+++ src/components/Incident.js
@@ -3,8 +3,8 @@ import Paper from "@material-ui/core/Paper";
 import { makeStyles, Card } from "@material-ui/core";
 import FlexColumn from "../Theme/FlexColumn";
 import FlexRow from "../Theme/FlexRow";
-import Avatar from '@material-ui/core/Avatar';
-import TwitterIcon from '@material-ui/icons/Twitter';
+import Avatar from "@material-ui/core/Avatar";
+import TwitterIcon from "@material-ui/icons/Twitter";
 
 const useStyles = makeStyles({
   root: {
@@ -15,28 +15,28 @@ const useStyles = makeStyles({
     display: "flex",
     justifyContent: "start",
     height: "100px",
-    backgroundColor: "#707070",
+    backgroundColor: "#212121",
   },
   avatar: {
     height: 120,
     width: 120,
     borderRadius: 10,
     margin: 10,
-  }, 
+  },
   info: {
-    display: 'flex',
-    alignItems: 'center',
-    alignItems: 'start',
+    display: "flex",
+    alignItems: "center",
+    alignItems: "start",
     paddingLeft: 10,
-    color: '#898989'
+    color: "#898989",
     // padding: 5
-  }, 
+  },
   title: {
-    color: 'white', 
-    fontWeight: 'bold',
-    fontSize: '20px'
-  }
-})
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "20px",
+  },
+});
 
 const Incident = (props) => {
   const classes = useStyles();
@@ -44,22 +44,24 @@ const Incident = (props) => {
     <Paper onClick={() => props.setCurrentIncident(props.incident)}>
       <Card className={classes.root}>
         <FlexColumn>
-            <Avatar variant="square" className={classes.avatar} alt="" src={props.incident.image_url}>
-          </Avatar>
+          <Avatar
+            variant="square"
+            className={classes.avatar}
+            alt=""
+            src={props.incident.image_url}
+          ></Avatar>
           {/* <FlexRow>{props.image_url}</FlexRow> */}
         </FlexColumn>
         <FlexColumn className={classes.info}>
-          <FlexRow >
-              {props.incident.date}
-          </FlexRow>
-          <FlexRow className={classes.title}>
-              {props.incident.title}
-          </FlexRow>
-          <FlexRow style={{overflow: 'auto', maxHeight: '80px', width: '275px'}}>
-              {props.incident.description}
+          <FlexRow>{props.incident.date}</FlexRow>
+          <FlexRow className={classes.title}>{props.incident.title}</FlexRow>
+          <FlexRow
+            style={{ overflow: "auto", maxHeight: "80px", width: "275px" }}
+          >
+            {props.incident.description}
           </FlexRow>
           <FlexRow>
-            <TwitterIcon style={{color: "#FCC42C"}}></TwitterIcon>
+            <TwitterIcon style={{ color: "#FCC42C" }}></TwitterIcon>
           </FlexRow>
         </FlexColumn>
       </Card>
