22a1f2ba9b3c0cf0567cc05a61792189e76a664f
Nathan Brickett <natey37@gmail.com>
2020-06-18 21:09:25 -0400

    add tweet buttom, form, tweet adds to incident, shows up when submitted

---
+++ src/components/TweetForm.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./actionForm.css";
+import CloseIcon from "@material-ui/icons/Close";
+import { postTweets } from "../requests/requests";
+
+class TweetForm extends React.Component {
+
+  state = {
+    url: "",
+    incident_id: ""
+  }
+
+  componentDidMount(){
+    this.setState({
+        incident_id: this.props.incident.id
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    console.log('u pressed submit'
+    )
+    event.preventDefault()
+    postTweets(this.state)
+    this.setState({
+        url: "",
+        incident_id: ""
+    })
+    this.props.tweetButton()
+
+  }
+
+  render() {
+    console.log(this.props)
+    console.log(this.state)
+
+    return (
+      <div id="actionForm" style={{color: 'black'}}>
+        <h2 style={{color: 'black'}}>Add a Tweet!</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="url" >
+              Url<span className="required">*</span>
+          </label>
+          <input
+            name="url"
+            type="text"
+            placeholder="Add a link to Twitter!"
+            value={this.state.url}
+            onChange={this.handleChange}
+          />
+
+          <input type="submit" value="Submit" id="submitButton" />
+          <label hidden>
+            Incident ID
+          </label>
+          <input
+           hidden
+           name='incidentID'
+           type="text"
+           value={this.state.incidentID}
+          >
+          </input>
+        </form>
+        <CloseIcon
+          id="close-button"
+          onClick={this.props.tweetButton}
+          style={{ cursor: "pointer" }}
+        />
+      </div>
+    );
+  }
+}
+
+export default TweetForm;
--- src/requests/requests.js
+++ src/requests/requests.js
@@ -2,7 +2,18 @@ export const baseURL = 'http://localhost:3000'
 
 const parseData = response => response.json()
 
-//this is the post to incidents 
+//GET REQUESTS
+
+//get to incidents
+export const getIncidents = () => fetch(`${baseURL}/incidents`).then(parseData)
+
+//get to incident with tweets
+export const getIncidentWithTweets = (id) => fetch(`${baseURL}/incidents/tweets/${id}`).then(parseData)
+
+
+//POST REQUESTS 
+
+//post to incidents 
 export const postIncidents = (data) => fetch(`${baseURL}/incidents`, {
     method: 'POST', 
     headers: {
@@ -13,12 +24,19 @@ export const postIncidents = (data) => fetch(`${baseURL}/incidents`, {
 })
 .then(parseData)
 
-export const getIncidents = () => fetch(`${baseURL}/incidents`).then(parseData)
-    
-export const getIncidentWithTweets = (id) => fetch(`${baseURL}/incidents/tweets/${id}`).then(parseData)
-
 //post to actions
 export const postActions = (data) => fetch(`${baseURL}/actions`, {
+    method: 'POST', 
+    headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+    },
+    body: JSON.stringify(data)
+})
+.then(parseData)
+
+//post to tweets
+export const postTweets = (data) => fetch(`${baseURL}/tweets`, {
     method: 'POST', 
     headers: {
         'content-type': 'application/json',
