f2231d1425d32a87f396452a0718340153468ceb
Steven Zhu <stevenzhu03@gmail.com>
2020-06-20 16:37:49 -0400

    adding passport for google oauth

---
+++ server/services/passport.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
+//user is what is pulled out of Mongo database
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => done(null, user));
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleID: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      //   console.log(profile);
+      const user = await new User({ googleID: profile.id }).save();
+      done(null, user);
+    }
+  )
+);
