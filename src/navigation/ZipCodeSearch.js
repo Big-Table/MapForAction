042d5243055e83bf5daa64be6faf2a6a89645383
Nathan Brickett <natey37@gmail.com>
2020-07-03 13:43:07 -0400

    finished zipcode search frontend with validations

--- src/navigation/ZipCodeSearch.js
+++ src/navigation/ZipCodeSearch.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from "@material-ui/core/TextField";
 import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from "@material-ui/core";
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles({
     root: {
@@ -21,30 +22,69 @@ const useStyles = makeStyles({
           "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
             borderColor: "blue"
           }
-        // color: 'white'
-        // color: "#FCC42C",
-        // borderStyle: "solid",
-        // borderColor: "#FCC42C",
-        // borderWidth: 1,
-        // height: 30,
-        // width: 200,
-        // borderRadius: 10,
-        // fontFamily: 'Work Sans',
-        // fontWeight: 700,
-        // cursor: "pointer",
-        // outline: "none",
-        // verticalAlign: 'center'
-
-    }
+    }, 
+    tooltip: {
+        backgroundColor: 'black', 
+        color: '#FCC42C',
+        fontSize: 13,
+        // height: '20px', 
+        // width: '10px'
+    }, 
+   
 })
 
 function ZipCodeSearch(){
 
     const classes = useStyles()
 
+    const [zip, setZip] = useState('')
+
+    const handleZip = (newZip) => {
+        setZip(newZip)
+    }
+
+    const handleZipcodeInputPaste = e => {
+        // test value: 123as8aa2129
+        e.preventDefault();
+    
+        // get pasted content
+        let pasteText = e.clipboardData.getData("text/plain");
+        // only allow integers
+        pasteText = pasteText.replace(/[^0-9]/g, "");
+        // add to current input value (target)
+        let newContent = e.target.value + pasteText;
+        // only allow 5 digits total
+        newContent = newContent.substring(0, 5);
+        // set new value of input
+        e.target.value = newContent;
+        console.log(newContent)
+        // set new state of zipcode
+        handleZip(newContent)
+    };
+
+    const handleZipCodeChange = (e) => {
+
+        const re = /^[0-9\b]+$/;
+
+        // if value is not blank, then test the regex
+        if(e.target.value.length <= 5){
+            if (e.target.value === '' || re.test(e.target.value)) {
+                setZip(e.target.value)
+            }
+        }
+    }
+
+    const onZipCodeSearchClick = () => {
+        console.log("hi")
+    }
+
+    console.log(zip)
     return(
         <div >
             <TextField 
+            onChange={e => handleZipCodeChange(e)}
+            onPaste={e => handleZipcodeInputPaste(e)}
+            value={zip}
             className={classes.root}
             style={{color: 'white'}}
             placeholder='Enter Zip Code'
@@ -52,9 +92,16 @@ function ZipCodeSearch(){
             variant="outlined"
             InputProps={{
                 style: {color: "#FCC42C"},
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
+                endAdornment: (
+                  <InputAdornment position="center">
+                    <Tooltip 
+                    classes={{
+                        tooltip: classes.tooltip,
+                        arrow: classes.arrow
+                      }}
+                    title='Search'>
+                        <SearchIcon cursor='pointer' onClick={onZipCodeSearchClick}/>
+                    </Tooltip>
                   </InputAdornment>
                  )
             }}
