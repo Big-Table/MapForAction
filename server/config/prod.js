cf8bcf1d176b62bb815fc673b3078fefd76c23f1
Nathan Brickett <natey37@gmail.com>
2020-07-28 14:38:35 -0400

    heroku

--- server/config/prod.js
+++ server/config/prod.js
@@ -5,4 +5,5 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY
 };
