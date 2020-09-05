ed2db9a938703edcf19ef58cae3105fb03ff3f1a
Steven Zhu <stevenzhu03@gmail.com>
2020-09-04 22:58:57 -0400

    removed unsued imports and fixed props

--- src/components/forms/DatePicker.js
+++ src/components/forms/DatePicker.js
@@ -1,12 +1,9 @@
-import { makeStyles } from "@material-ui/core";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-
-const useStyles = makeStyles({});
 
 function Example(props) {
   const [startDate, setStartDate] = useState(new Date());
